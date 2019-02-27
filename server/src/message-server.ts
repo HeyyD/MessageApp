4697818fda805af808f2e2ba0030d412fd1bb205
HeyyD <heidi.hati@cs.tamk.fi>
2019-02-27 20:15:58 +0200

    start server

--- server/src/message-server.ts
+++ server/src/message-server.ts
@@ -5,15 +5,22 @@ export class MessageServer {
   private app: express.Application;
   private server: Server;
 
+  private port = 8080;
+
   constructor() {
     this.app = express();
     this.server = createServer(this.app);
-    this.server.listen(8080, () => {
-      console.log('The server is running in port 8080');
-    })
+
+    this.startServer();
   }
 
   getApp(): express.Application {
     return this.app;
+  }
+
+  private startServer(): void {
+    this.server.listen(this.port, () => {
+      console.log(`The server is running in port ${this.port}`);
+    });
   }
 }
