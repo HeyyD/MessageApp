1a0e32c4f86661403901ed7761e3cc05642eec2b
HeyyD <heidi.hati@cs.tamk.fi>
2019-02-28 18:32:17 +0200

    connect to websocket

--- client/src/services/MessageManager.tsx
+++ client/src/services/MessageManager.tsx
@@ -1,8 +1,13 @@
+import SocketIOClient from 'socket.io-client';
+
 export default class MessageManager {
 
   private static instance: MessageManager;
+  private socket: any;
 
-  private constructor() {}
+  private constructor() {
+    this.socket = SocketIOClient('ws://192.168.1.31:8080');
+  }
 
   static getInstance(): MessageManager {
     if (!MessageManager.instance) {
