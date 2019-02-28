9d611cf56e25178f506085af63fdd855f32d98c3
HeyyD <heidi.hati@cs.tamk.fi>
2019-02-28 16:46:32 +0200

    add websocket to server

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,20 @@
 import { createServer, Server } from 'http';
 import * as express from 'express';
+import * as socketio from 'socket.io';
 
 class MessageServer {
   
   private app: express.Application;
   private server: Server;
+  private websocket: socketio.Server;
 
+  private address = '192.168.1.31'
   private port = 8080;
 
   constructor() {
     this.app = express();
     this.server = createServer(this.app);
+    this.websocket = socketio(this.server);
 
     this.startServer();
   }
@@ -21,7 +25,11 @@ class MessageServer {
 
   private startServer(): void {
     this.server.listen(this.port, () => {
-      console.log(`The server is running in port ${this.port}`);
+      console.log(`The server is running in ${this.address}:${this.port}`);
+
+      this.websocket.on('connection', (socket) => {
+        console.log(`Client connected: ${socket.id}`);
+      });
     });
   }
 }
