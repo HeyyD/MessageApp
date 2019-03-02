87a080a39d7e582896449a7dc20c47fc2ab3c36f
HeyyD <heidi.hati@cs.tamk.fi>
2019-03-02 13:30:30 +0200

    Display messages in chat component

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import MessageManager from '../services/MessageManager';
+import { Message } from '../models/Message';
 
-export default class Chat extends Component {
+interface Props {}
+interface State {
+  messages: Message[]
+}
+export default class Chat extends Component<Props, State> {
+
+  private messageManager: MessageManager;
+  private messages: Message[] = [];
+
+  constructor(props: Props) {
+    super(props);
+    this.messageManager = MessageManager.getInstance();
+
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    this.messageManager.onMessage().subscribe(message => {
+      this.messages.push(message);
+      this.setState({
+        messages: [...this.messages]
+      });
+    });
+  }
+
   render() {
     return (
-      <Text>Chat Component</Text>
-    )
+      <Text>{ this.state.messages.map(m => m.text) }</Text>
+    );
   }
 }
