c080ef65d00ccd3dad8e5180f85dbd6ca3110f3e
HeyyD <heidi.hati@cs.tamk.fi>
2019-03-04 18:18:35 +0200

    empty text input after send

--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -13,17 +13,29 @@ export default class ChatInput extends Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
+
+    this.sendMessage = this.sendMessage.bind(this);
+
     this.messageManager = MessageManager.getInstance();
     this.state = {
       message: ''
     }
   }
 
+  sendMessage() {
+    this.messageManager.sendMessage({text: this.state.message});
+    this.setState({message: ''});
+  }
+
   render() {
     return (
       <View>
-        <TextInput style={{backgroundColor: '#ebebeb'}} onChangeText={(text: string) => this.setState({message: text})} />
-        <Button title='send' onPress={ () =>  this.messageManager.sendMessage({text: this.state.message})}/>
+        <TextInput 
+          style={{backgroundColor: '#ebebeb'}}
+          onChangeText={(text: string) => this.setState({message: text})}
+          value={ this.state.message }
+        />
+        <Button title='send' onPress={ () => this.sendMessage()}/>
       </View>
     )
   }
