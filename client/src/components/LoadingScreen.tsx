a2850720472551f5786fded2f152ac2cb7ce7ac2
HeyyD <heidi.hati@cs.tamk.fi>
2019-03-06 15:43:49 +0200

    go to register if user was not found

--- client/src/components/LoadingScreen.tsx
+++ client/src/components/LoadingScreen.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { Text, View, ActivityIndicator, StyleSheet, AsyncStorage } from 'react-native';
+import { NavigationScreenProps, NavigationScreenProp } from 'react-navigation';
 
 let styles = StyleSheet.create({
   container: {
@@ -13,7 +14,31 @@ let styles = StyleSheet.create({
   }
 })
 
-export default class LoadingScreen extends Component {
+interface Props extends NavigationScreenProps {}
+
+export default class LoadingScreen extends Component<Props> {
+
+  componentDidMount() {
+    this.init();
+  }
+
+  async init(): Promise<void> {
+    try {
+      const user = await AsyncStorage.getItem('USER');
+      if (user !== null) {
+        console.log('user found');
+      } else {
+        console.log('user not found')
+        // this.props.navigation.navigate('Register');
+        setTimeout(() => {
+          this.props.navigation.replace('Register');
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
