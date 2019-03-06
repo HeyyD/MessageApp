75ecbd20d6df85d940c42a432cc9f5f9b2fecd2f
HeyyD <heidi.hati@cs.tamk.fi>
2019-03-06 22:18:24 +0200

    init post route for users

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { User } from '../models/user';
 
 export class Users {
   private router = express.Router();
@@ -7,6 +9,13 @@ export class Users {
     this.router.get('/', (req: express.Request, res: express.Response) => {
       res.send({hello: 'Hello World'});
     });
+
+    this.router.post('/', (req: express.Request, res: express.Response) => {
+      const data = req.body as User;
+      res.send(data);
+    });
+
+    app.use(bodyParser.json());
     app.use('/api/users', this.router);
   }
 }
