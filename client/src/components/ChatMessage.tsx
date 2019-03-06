7cf056cd5f84edb84486f4fb724b182518d8ee9d
HeyyD <heidi.hati@cs.tamk.fi>
2019-03-06 18:00:33 +0200

    message styling according to user

--- client/src/components/ChatMessage.tsx
+++ client/src/components/ChatMessage.tsx
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MessageManager from '../services/MessageManager';
+import { Message } from '../models/Message';
 
 let styles = StyleSheet.create({
   container: {
+    flexShrink: 1,
+    alignSelf: 'flex-start',
     backgroundColor: '#ebebeb',
     padding: 10,
-    marginVertical: 10,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 'auto',
+    margin: 10,
+    borderRadius: 10
   },
   text: {
-    fontSize: 15
+    fontSize: 15,
+    color: 'rgba(0, 0, 0, 1)'
+  },
+  username: {
+    fontSize: 10,
   },
   ownMessage: {
-    marginLeft: 'auto',
-    marginRight: 10,
-    backgroundColor: '#ffA500'
+    margin: 10,
+    alignSelf: 'flex-end',
+    backgroundColor: '#ffA500',
   }
 });
 
 interface Props {
-  text: string
+  message: Message
 }
 export default class ChatMessage extends Component<Props> {
+
+  private messageManager: MessageManager;
+
   constructor(props: Props) {
     super(props);
+    this.messageManager = MessageManager.getInstance();
   }
   render() {
+    const user = this.messageManager.getUser();
+    const sender = this.props.message.user;
+
     return (
-      <View style={ [styles.container, styles.ownMessage] }>
-        <Text style={ styles.text }>{ this.props.text }</Text>
+      <View style={ [styles.container, (user === sender) ? styles.ownMessage : null] }>
+        <Text style={ styles.text }>{ this.props.message.text }</Text>
+        <Text style={[styles.username, (user === sender) ? {textAlign: 'right'} : null ]}>{ this.props.message.user }</Text>
       </View>
     );
   }
