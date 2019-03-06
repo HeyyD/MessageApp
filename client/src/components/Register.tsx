999f44211406b3450c1d10864136e6d795eef5f5
HeyyD <heidi.hati@cs.tamk.fi>
2019-03-06 16:28:21 +0200

    Register view layout

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, TextInput, StyleSheet, Button } from 'react-native';
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    margin: 10
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#eb6123'
+  },
+  insturctions: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginVertical: 10
+  },
+  input: {
+    backgroundColor: '#ebebeb',
+    borderRadius: 20,
+    padding: 10
+  },
+  button: {
+    marginVertical: 10,
+    alignSelf: 'center'
+  }
+});
 
 export default class Register extends Component {
   render() {
     return(
-      <Text>Register working!</Text>
+      <View style={ styles.container }>
+        <Text style={ styles.welcome }>Welcome!</Text>
+        <Text style={ styles.insturctions }>You seem to be a new user. Please give us your name</Text>
+        <TextInput
+          style={ styles.input }
+          placeholder='Username'
+        />
+        <View style={ styles.button }>
+          <Button title='Register' onPress={ () => console.log('Button pressed') } color='#eb6123'/>
+        </View>
+      </View>
     );
   }
 }
